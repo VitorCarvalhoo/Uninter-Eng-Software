42a170650d969d9890ea47c3c3f86eea52ab480f
Vitor G. Carvalho <78125536+VitorCarvalhoo@users.noreply.github.com>
2025-03-12 19:53:34 -0300

    Adição de database p/ estudos

--- projetos/02/script.js
+++ projetos/02/script.js
@@ -52,15 +52,16 @@ function calcular() {
         const valor1 = parseInt(campo1.value);
         const valor2 = parseInt(campo2.value);
         const operacao = seletor.value;
-
-        if(operacao === 'Somar') {
-            resposta.innerHTML = valor1 + valor2;
-        } if(operacao === 'Subtrair') {
-            resposta.innerHTML = valor1 - valor2;
-        } if(operacao === 'Dividir') {
-            resposta.innerHTML = valor1 / valor2;
-        } if(operacao === 'Multiplicar') {
-            resposta.innerHTML = valor1 * valor2;
+        switch(operacao)
+        {
+            case operacao === "Somar":
+                resposta.innerHTML = valor1 + valor2;
+            case operacao === "Subtrair":
+                resposta.innerHTML = valor1 - valor2;
+            case operacao === "Dividir":
+                resposta.innerHTML = valor1 / valor2;
+            case operacao === "Multiplicar":
+                resposta.innerHTML = valor1 * valor2;
         }
     }
 }
